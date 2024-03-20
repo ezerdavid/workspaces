e901532bda85782a7e7f82469493e9265637c3d4
David Ezer <david.ezer@statsperform.com>
2024-03-20 18:41:47 +0100

    Parametrize open command

--- open.py
+++ open.py
@@ -1,7 +1,17 @@
 import os
 
+
+
 os.system("shopt -s extglob")
 os.system(r"open /Applications/Microsoft\ Teams\ \(work\ or\ school\).app")
 os.system("open /Applications/WhatsApp.app")
 
+
+apps = [r"/Applications/Microsoft\ Teams\ \(work\ or\ school\).app", "/Applications/WhatsApp.app"]
+
+def open(applications: list[str]):
+    OPEN_COMMAND = "open"
+    for app in applications:
+        command = f"{OPEN_COMMAND} {app}"
+        os.system(command)
 
