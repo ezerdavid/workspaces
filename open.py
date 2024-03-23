cf3e0e8630d103e45eda910bbace895d303ff30e
David Ezer <david.ezer@statsperform.com>
2024-03-23 11:26:23 +0100

    Add delete option

--- open.py
+++ open.py
@@ -9,15 +9,16 @@ APPLICATIONS_BASE = "/Applications/"
 SAVED_BASE = "./saved"
 
 # TODO: Create menu
-#           - Open apps
 #           - Delete apps
 #           - Load apps from /Applications/ (not sure if brew add there as well -> check) (D) -> Didn't check the brew
 # open workspace from command line on arg[0] available
-def open_applications(applications: list[str]):
+def open_applications(name: str, applications: list[str]):
     OPEN_COMMAND = "open"
 
+    print(f"Opening workspace {name}")
     for app in applications:
-        command = f"{OPEN_COMMAND} {app}"
+        # TODO handle if application not present -> For example uninstalled
+        command = f"{OPEN_COMMAND} {APPLICATIONS_BASE}/{app}"
         os.system(command)
 
 def available_applications():
@@ -42,7 +43,7 @@ def application_names(apps: list[str]):
 
 def render_main_menu():
     os.system('clear')
-    options = ["[a] Add workspace", "[c] Change Workspace", "[q] Quit"]
+    options = ["[o] Open workspace", "[a] Add workspace", "[c] Change workspace", "[d] Delete workspace", "[q] Quit"]
     main_menu = TerminalMenu(options)
     main_menu_entry_index = main_menu.show()
     selected_option = options[main_menu_entry_index]
@@ -51,16 +52,29 @@ def render_main_menu():
         sys.exit(0)
     elif selected_option == "[a] Add workspace":
         add_workspace(None, [])
-    elif selected_option == "[c] Change Workspace":
-        change_workspace()
+    elif selected_option == "[c] Change workspace":
+        handle_workspace(add_workspace)
+    elif selected_option == "[o] Open workspace":
+        handle_workspace(open_applications)
+    elif selected_option == "[d] Delete workspace":
+        handle_workspace(delete_workspace)
 
-def change_workspace():
+def handle_workspace(handler):
     workspaces = list_workspaces()
     change_menu = TerminalMenu(workspaces)
     change_indicies = change_menu.show()
     workspace = workspaces[change_indicies]
     workspace_apps = load_workspace(workspace)
-    add_workspace(workspace, application_names(workspace_apps))
+    handler(workspace, workspace_apps)
+
+def delete_workspace(name: str, applications: list[str]):
+    app_names = application_names(applications)
+    print(name)
+    print(*app_names, sep="\n")
+    confirmed = input("Do you wish to delete above workspace? [y/yes] ")
+    if confirmed.lower() in ('y', 'yes'):
+        Path(f"{SAVED_BASE}/{name}").unlink()
+    render_main_menu()
 
 def list_workspaces():
     workspaces = Path(f"{SAVED_BASE}/") 
@@ -74,11 +88,12 @@ def load_workspace(workspace_name: str):
             entries.append(line.strip())  # Remove leading/trailing whitespace and newline characters
     return entries
 
-def add_workspace(name: str | None, workspace_apps_names: list[str]):
+def add_workspace(name: str | None, workspace_apps: list[str]):
     choosen_name = name if name is not None else choose_name()
 
     applications = available_applications()
     names = application_names(applications)
+    workspace_apps_names = application_names(workspace_apps)
 
     application_list_terminal = TerminalMenu(names, multi_select=True, show_multi_select_hint=True, preselected_entries=workspace_apps_names, multi_select_select_on_accept=False)
     applications_indicies = application_list_terminal.show()
